fc30d0f1dee64fb095e1ba2c56202070fc66bec0
Tsimofei Piatrou <timap446@gmail.com>
2022-04-14 17:25:00 +0300

    utils

---
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -0,0 +1,35 @@
+package utils;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class CapabilitiesGenerator {
+
+    public static final String RESOURCE_PATH = "src/test/resources";
+    public static final File RESOURCE_PATH_FILE = new File(RESOURCE_PATH);
+    public static final String ABSOLUTE_RESOURCE_PATH = RESOURCE_PATH_FILE.getAbsolutePath();
+
+    public static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        String os = System.getProperty("os.name").toLowerCase();
+        String driver_path = PropertiesUtils.getEnv("driver_path");
+        System.out.println("Operational system: " + os + "; Driver path: " + driver_path);
+        System.setProperty("webdriver.chrome.driver", driver_path);
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--headless"); // only if you are ACTUALLY running headless
+        options.addArguments("--start-maximized");
+        //options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        //options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        //options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        //options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("download.default_directory", ABSOLUTE_RESOURCE_PATH);
+        options.setExperimentalOption("prefs", chromePrefs);
+        return options;
+    }
+
+}
